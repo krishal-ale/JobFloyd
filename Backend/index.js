54dc8feb1b30b14a095ca6bc7bc81205717b1db3
Krishal Ale <alekrishal@gmail.com>
2026-02-05 18:49:04 +0545

    backend folder added

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
