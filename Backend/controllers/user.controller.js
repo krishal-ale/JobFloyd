8ded7041f3a813929d247bef731081efd0dab539
Krishal Ale <alekrishal@gmail.com>
2026-02-06 00:47:37 +0545

    user registration controller completed

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,2 +1,23 @@
+import User from "../models/user.model.js";
+import bcrypt from 'bcrypt';
 
 
+export const register = async (req ,res)=>{
+    try {
+        const {fullname,email,phoneNumber,password,role} = req.body;
+        if(!fullname || !email || !phoneNumber || !password || !role){
+            return res.status(400).json({message:"All fields are required", success:false});
+        };
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.status(400).json({message:"User already exists", success:false});
+        };
+        const hashedPassword = await bcrypt.hash(password,10);
+        const newUser = new User({fullname,email,phoneNumber,password:hashedPassword,role});
+        await newUser.save();
+        return res.status(201).json({message:"User registered successfully", success:true});
+
+    } catch (error) {
+        
+    }
+}
