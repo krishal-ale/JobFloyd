05443e6f27bd2f9409f6c49538da1d0af628ff92
Krishal Ale <alekrishal@gmail.com>
2026-05-03 22:06:44 +0545

    mail error done

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,22 +1,32 @@
 import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+import { Resend } from "resend";
 
 const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"JobFloyd" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+  if (process.env.NODE_ENV === "production") {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    await resend.emails.send({
+      from: "JobFloyd <onboarding@resend.dev>",
+      to,
+      subject,
+      html,
+    });
+  } else {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+    await transporter.sendMail({
+      from: `"JobFloyd" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+  }
 };
 
 export default sendEmail;
